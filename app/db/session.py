0cc4188a289e941d61c8321cbdad48a8e9fadb9b
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2025-07-24 18:22:30 -0500

    feat(db): add db session module

---
+++ app/db/session.py
@@ -0,0 +1,80 @@
+"""
+Database session script
+"""
+
+import logging
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import (
+	AsyncEngine,
+	AsyncSession,
+	async_sessionmaker,
+	create_async_engine,
+)
+
+from app.config.config import sql_database_setting
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+url: str = f"{sql_database_setting.POSTGRES_URL}"
+
+async_engine: AsyncEngine = create_async_engine(
+	url,
+	pool_pre_ping=True,
+	future=True,
+	echo=True,
+	pool_size=10,
+	max_overflow=20,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+	bind=async_engine,
+	autoflush=False,
+	expire_on_commit=False,
+	class_=AsyncSession,
+)
+
+
+@asynccontextmanager
+async def get_session() -> AsyncGenerator[AsyncSession]:
+	"""
+	Yield an asynchronous session to the database.
+
+	Yields:
+		AsyncGenerator[AsyncSession]: The session yielded.
+
+	Raises:
+		Exception: If the database connection fails.
+	"""
+	async with AsyncSessionLocal() as session:
+		try:
+			yield session
+			await session.commit()
+		except Exception as e:
+			logger.error("Session rollback due to exception: %s", e)
+			await session.rollback()
+			raise
+
+
+async def check_db_health(session: AsyncSession) -> bool:
+	"""
+	Check the health of the database connection.
+
+	Args:
+		session (AsyncSession): The SQLAlchemy asynchronous session object used to interact with the database.
+
+	Returns:
+		bool: True if the database connection is healthy, False otherwise.
+
+	Raises:
+		SQLAlchemyError: If the database connection fails.
+	"""
+	try:
+		await session.execute(text("SELECT 1"))
+		return True
+	except SQLAlchemyError as e:
+		logger.error("Database connection error: %s", e)
+		return False
--- app/models/structured/__init__.py
+++ app/models/structured/__init__.py
@@ -1,3 +1,11 @@
 """
 Package app.models.structured initialization.
 """
+
+from app.models.base_class import Base
+from app.models.structured.order import Order
+from app.models.structured.order_item import OrderItem
+from app.models.structured.product import Product
+from app.models.structured.user import User
+
+__all__: list[Base] = [User, Product, Order, OrderItem]
