efac49f62d12bdf3f8c9c6378f7dab1f2c335249
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2025-07-30 13:09:55 -0500

    feat(repository): add soft delete method for product repository

--- app/repositories/structured/product_repository.py
+++ app/repositories/structured/product_repository.py
@@ -3,7 +3,7 @@ Provides CRUD operations for the Product model.
 """
 
 from pydantic import UUID4
-from sqlalchemy import Result, Select, select
+from sqlalchemy import CursorResult, Executable, Result, Select, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.structured.product import Product
@@ -59,3 +59,23 @@ class ProductRepository(BaseRepository[Product]):
 		stmt: Select = select(self.model).where(self.model.name == name)
 		result: Result = await self.session.execute(stmt)
 		return result.scalar_one_or_none()
+
+	async def soft_delete(self, _id: UUID4) -> bool:
+		"""
+		Softly delete a Product by their UUID4 through marking its is_active column as False.
+
+		Args:
+			_id (UUID4): The Product's UUID4.
+
+		Returns:
+			bool: True if soft deletion was successful, False otherwise.
+		"""
+		stmt: Executable = (
+			update(self.model)
+			.where(self.model.id == _id)
+			.values(is_active=False)
+			.execution_options(synchronize_session="fetch")
+		)
+		result: Result | CursorResult = await self.session.execute(stmt)
+		await self.session.commit()
+		return result.rowcount() > 0
