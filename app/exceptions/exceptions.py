bfd5da09bb6c0accb44da7d670d3b6a14e62b65c
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2025-07-30 19:05:09 -0500

    feat(exceptions): add a custom exception for security failing cases

--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -12,3 +12,14 @@ class ServiceException(HTTPException):
 		super().__init__(status.HTTP_503_SERVICE_UNAVAILABLE, message)
 		if note:
 			self.add_note(note)
+
+
+class SecurityException(HTTPException):
+	"""Security Exception class"""
+
+	def __init__(self, message: str, note: str | None = None):
+		super().__init__(
+			status.HTTP_511_NETWORK_AUTHENTICATION_REQUIRED, message
+		)
+		if note:
+			self.add_note(note)
